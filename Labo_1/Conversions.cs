884ecd882002a9347ab58f02fd3fa559a5b3f908
Edwin J. Arias <edwin.arias@jala.university>
2025-01-09 15:34:32 -0500

    Agregar archivos de proyecto.

---
+++ Labo_1/Conversions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_1
+{
+    internal class Conversions
+    {
+        public short IntToShort(int value)
+        {
+            short shortValue;
+
+            if (value < short.MinValue || value > short.MaxValue)
+            {
+                Console.WriteLine($"Error to convert {value} to short. Value is out of range.");
+                return 0; 
+            }
+
+            shortValue = (short)value;
+            Console.WriteLine($"{value.GetType()} -> {shortValue.GetType()}");
+
+            return shortValue;
+        }
+
+
+        public long IntToLong(int value)
+        {
+            long longValue = value;
+            Console.WriteLine($"{value.GetType()} -> {longValue.GetType()}");
+
+            return longValue;
+        }
+
+        public int FloatToInt(float value)
+        {
+            if (value < int.MinValue || value > int.MaxValue)
+            {
+                Console.WriteLine($"Error to convert {value} to int. Value is out of range.");
+                return 0;
+            }
+            int intValue = (int)value;
+            Console.WriteLine($"{value.GetType()} -> {intValue.GetType()}");
+
+            return intValue;
+        }
+
+        public float IntToFloat(int value)
+        {
+            float floatValue = (float)value;
+            Console.WriteLine($"{value.GetType()} -> {floatValue.GetType()}");
+            return floatValue;
+        }
+
+        public double IntToDouble(int value)
+        {
+            double doubleValue = value;
+            Console.WriteLine($"{value.GetType()} -> {doubleValue.GetType()}");
+
+            return doubleValue;
+        }
+        
+        public string BoolToString(bool value)
+        {
+            var str = value.ToString();
+            Console.WriteLine($"{value.GetType()} -> {str.GetType()}");
+
+            return str;
+        }
+        
+        public string IntToString(int value)
+        {
+            string str = value.ToString();
+            Console.WriteLine($"{value.GetType()} -> {str.GetType()}");
+
+            return value.ToString();
+        }
+
+        public object DateTimeToObject(DateTime value)
+        {
+            object boxedValue = value;
+            Console.WriteLine($"Boxing (DateTime a object): DateTime -> object");
+            return boxedValue;
+        }
+
+        public DateTime ObjectToDateTime(object value)
+        {
+            DateTime unboxedValue = (DateTime)value;
+            Console.WriteLine($"Unboxing (object a DateTime): object -> DateTime");
+            return unboxedValue;
+        }
+
+
+
+    }
+}
---
+++ Labo_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Labo_1;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("----- Conversiones -----");
+        var conversions = new Conversions();
+
+        int.TryParse(args[0], out int intToShort);
+        conversions.IntToShort(intToShort);
+
+        int.TryParse(args[1], out int intToLong);
+        conversions.IntToLong(intToLong);
+
+        float.TryParse(args[2], out float floatToInt);
+        conversions.FloatToInt(floatToInt);
+
+        int.TryParse(args[3], out int intToFloat);
+        conversions.IntToFloat(intToFloat);
+
+        int.TryParse(args[4], out int intToDouble);
+        conversions.IntToDouble(intToDouble);
+
+        bool boolToString = bool.Parse(args[5]);
+        conversions.BoolToString(boolToString);
+
+        int intToString = int.Parse(args[6]);
+        conversions.IntToString(intToString);
+
+        DateTime.TryParse(args[7], out DateTime boxingValue);
+        object boxed = conversions.DateTimeToObject(boxingValue);
+
+        DateTime.TryParse(args[8], out DateTime parsedDateTime);
+        object objectToUnbox = parsedDateTime;
+        conversions.ObjectToDateTime(objectToUnbox);
+        
+       
+
+    }
+    
+}
+//dotnet run 123 456 789.12 12345 67890 true 42 "2025-01-09T10:30:00" "2025-01-09T15:45:00"
